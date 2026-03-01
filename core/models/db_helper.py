8cc85984f04df5857ebc8ec2593cdce10dddfdf6
zyxweqx <nazyabarya228@gmail.com>
2026-03-01 14:32:52 +0100

    create engine for sqlalchemy

--- core/models/db_helper.py
+++ core/models/db_helper.py
@@ -1,5 +1,12 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+    AsyncSession,
+)
+
+from asyncio import current_task
+
 
 from core.config import settings
 
@@ -16,6 +23,23 @@ class DatabaseHelper:
             autocommit=False,
             expire_on_commit=False,
         )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
+
+    async def scoped_session_dependency(self) -> AsyncSession:
+        session = self.get_scoped_session()
+        yield session
+        await session.close()
 
 
 db_helper = DatabaseHelper(
